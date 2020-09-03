b9fdd2e09c07cf043a69c5b32f4e4734b98d11b5
MuhammedSamy <1501214@eng.asu.edu.eg>
2020-09-03 21:24:49 +0200

    added photo-roll & competition

---
+++ src/app/photo-roll/photo-roll.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-roll',
+  templateUrl: './photo-roll.component.html',
+  styleUrls: ['./photo-roll.component.css']
+})
+export class PhotoRollComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
