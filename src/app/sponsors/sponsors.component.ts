224ff226338202cae25a84a68ccb8f91d789e9b5
Bassant-Yasser <bassantyasser229@gmail.com>
2020-09-08 06:14:44 +0200

    add admin panel sponsors edit

--- src/app/sponsors/sponsors.component.ts
+++ src/app/sponsors/sponsors.component.ts
@@ -13,9 +13,9 @@ export class SponsorsComponent implements OnInit {
   }
 
   sponsorsInfo = [
-    {logo : "assets/img/قادر.png", name : "Arab Organization for Industrialization" , desc : 'KADER factory for developed industries was established in 1949 under the name of "HELIOPOLIS AIRCRAFT FACTORY" to produce the primary training Aircraft ..'},
-    {logo : "assets/img/اليو.png", name : "ALUMISR", desc : "Alumisr company produces aluminum for the purposes of the various use with many finishes.It was established in 1977"},
-    {logo : "assets/img/اعمار.png", name : "EMAR", desc : "Emaar Misr is one of the largest real estate companies in Egypt and the developer of iconic projects such as Uptown Cairo, Marassi, and Mivida"}
+    {logo : "assets/img/kader.png", name : "Arab Organization for Industrialization" , desc : 'KADER factory for developed industries was established in 1949 under the name of "HELIOPOLIS AIRCRAFT FACTORY" to produce the primary training Aircraft ..'},
+    {logo : "assets/img/alumisr.png", name : "ALUMISR", desc : "Alumisr company produces aluminum for the purposes of the various use with many finishes.It was established in 1977"},
+    {logo : "assets/img/emar.png", name : "EMAR", desc : "Emaar Misr is one of the largest real estate companies in Egypt and the developer of iconic projects such as Uptown Cairo, Marassi, and Mivida"}
   ]
 
 }
