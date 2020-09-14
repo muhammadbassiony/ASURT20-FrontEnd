2c4571ea34623347345cb4342a224a0e64fbca51
Muhammad Bassiony <31866582+muhammadbassiony@users.noreply.github.com>
2020-09-14 04:12:49 +0200

    code review tasks 1 modified

--- src/app/landing-page/photoroll.model.ts
+++ src/app/landing-page/photoroll.model.ts
@@ -2,11 +2,14 @@ export class Photoroll {
   // imagePath1: string;
   // imagePath2: string;
   // imagePath3: string;
+  public photorollId: string;
+  public photorollName: string;
   public imagePaths:  string[] ;
-  public noPhotos:number;
+  public noPhotos: number;
 
-  constructor(number:number, imagePaths:string[]) {
+  constructor(number: number, imagePaths: string[]) {
+    // this.
     this.imagePaths = imagePaths;;
     this.noPhotos = number;
-}
+  }
 }
