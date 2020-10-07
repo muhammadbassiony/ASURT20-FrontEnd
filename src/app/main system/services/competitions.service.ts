faf3bcb8c90255a35e41812d1a458ee456213511
Muhammad Bassiony <31866582+muhammadbassiony@users.noreply.github.com>
2020-10-07 05:40:15 +0200

    manage comps functionality integrated in header

--- src/app/main system/services/competitions.service.ts
+++ src/app/main system/services/competitions.service.ts
@@ -76,7 +76,7 @@ export class CompetitionsService {
       awards: comp.awards,
       photoroll: comp.photoroll
     };
-    console.log('CMP SRVC - UPDATE CMP :: \n', competition);
+    
     return this.http.put(
       backend_uri + '/main/competitions/update-comp/' + compId,
       { ...competition },
