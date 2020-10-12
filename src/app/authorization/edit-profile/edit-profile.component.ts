0e29d4bf181888aebd9f0dcb41ab8cab8c4a33fd
Muhammad Bassiony <31866582+muhammadbassiony@users.noreply.github.com>
2020-10-12 07:46:29 +0200

    edit-profile fully connected to form

--- src/app/authorization/edit-profile/edit-profile.component.ts
+++ src/app/authorization/edit-profile/edit-profile.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import {FormBuilder, 
+  FormControl, 
+  FormGroup, 
+  Validators, 
+  ReactiveFormsModule, 
+  RequiredValidator, 
+  FormArray} from '@angular/forms';
+
 import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 import { take } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
+import { Observable, pipe, Subscription } from 'rxjs';
 
 import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 
 import { AuthUser } from '../authUser.model';
+import { User } from '../user.model';
 
 import { UserService } from '../user.service';
 
@@ -22,8 +32,10 @@ export class EditProfileComponent implements OnInit {
   userId: string;
   profileForm: FormGroup;
   authUser: AuthUser;
+  user: User;
 
   constructor(
+    private fb: FormBuilder,
     private usersService: UserService,
     private route: ActivatedRoute,
     private router: Router
@@ -37,11 +49,31 @@ export class EditProfileComponent implements OnInit {
       // console.log('EDIT-PROFILE :: AUTHUSER :: \n', this.authUser);
     })
     console.log('EDIT PROFILE USERID :: ', this.userId);
+
+    this.profileForm = this.fb.group({
+      'name': [ , [Validators.required, Validators.minLength(8)]], 
+      'mobile': [ , [Validators.required, Validators.pattern("[0-9 ]{11}")]], 
+      'gender': [ , [Validators.required]], 
+      'birthDate': [ , [Validators.required]], 
+      'university': [ , [Validators.required, Validators.minLength(3)]], 
+      'faculty': [ , [Validators.required]], 
+      'department': [ , [Validators.required]], 
+      'gradYear': [ , [Validators.required, Validators.minLength(4)]], 
+      'credit': [ , [Validators.required]], 
+      'collegeId': [ , [Validators.required, Validators.minLength(4)]], 
+      'emrgName': [ , [Validators.required]], 
+      'emrgMobile': [ , [Validators.required, Validators.pattern("[0-9 ]{11}")]], 
+      'emrgRelation': [ , [Validators.required]] 
+    });
   }
 
   onSubmit(profileForm: FormGroup){
     console.log('EDIT PROFILE FORM :: \n', this.profileForm);
-    this.usersService.authUser.next(this.authUser);
-    this.router.navigate(['dashboard']);
+    this.user = this.profileForm.value;
+    let ngbDate = this.profileForm.value.birthDate;
+    this.user.birthDate = new Date(ngbDate.year, ngbDate.month-1, ngbDate.day);
+    console.log('USERRRRRR FROM FORM :: \n', this.user);
+    // this.usersService.authUser.next(this.authUser);
+    // this.router.navigate(['dashboard']);
   }
 }
