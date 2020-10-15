e594a86e8e22e1df46a4c0903074536efae9ebc1
MuhammedSamy <1501214@eng.asu.edu.eg>
2020-10-15 02:13:38 +0200

    edit single user view component to be responsive

--- src/app/authorization/admin-guard.service.ts
+++ src/app/authorization/admin-guard.service.ts
@@ -1,11 +1,11 @@
 import {
-  ActivatedRouteSnapshot, 
-  CanActivate, 
-  Router, 
-  RouterStateSnapshot, 
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
   UrlTree } from '@angular/router';
 
-import { Observable } from "rxjs"; 
+import { Observable } from "rxjs";
 import { UserService } from './user.service';
 import { map, take } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
@@ -27,26 +27,26 @@ export class AdminGuardService implements CanActivate {
             return this.router.createUrlTree(['/sign-in']);
           }
 
-          
-           if(!user.profileComplete && route.routeConfig.path != 'edit-profile'){
+
+           /*if(!user.profileComplete && route.routeConfig.path != 'edit-profile'){
             return this.router.createUrlTree(['edit-profile']);
-          }
+          }*/
 
           let userLevel = user.level;
           console.log('AUTH GUARD HERE :: ROUTEDATA, USERLEVEL ::  ', route.routeConfig.path);
             // route.data.accessLevel, userLevel, route.data.accessLevel <= userLevel, <Boolean>route.data.accessLevel,
             // route.data.accessLevel && route.data.accessLevel <= userLevel);
-          
+
           if(route.data.accessLevel!= null && route.data.accessLevel <= userLevel){
             // //users access level is equal to that of allowed by this route
             // if(!user.profileComplete && route.routeConfig.path != 'edit-profile')  return this.router.createUrlTree(['/edit-profile']);
             // // console.log('AUTH GUARD RETURNING TRUE HERE\n');
             return true;
           }
-          
+
           return this.router.createUrlTree(['/']);
         })
       );
-      
+
   }
 }
