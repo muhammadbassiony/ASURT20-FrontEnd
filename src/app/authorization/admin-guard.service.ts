8030220554064c560947024dc2ea11ac94408c3f
Omar-Ashinawy <omarashinawy@gmail.com>
2020-10-22 23:08:06 +0200

    Added error response out of higher levels of access

--- src/app/authorization/admin-guard.service.ts
+++ src/app/authorization/admin-guard.service.ts
@@ -9,13 +9,14 @@ import { Observable } from "rxjs";
 import { UserService } from './user.service';
 import { map, take } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+import {ErrorService} from "../shared/errorModal/error.service";
 
 
 @Injectable({providedIn: 'root'})
 export class AdminGuardService implements CanActivate {
 
 
-  constructor(private usersService: UserService, private router: Router) {}
+  constructor(private usersService: UserService, private router: Router, private errorService: ErrorService) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
     Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
@@ -27,7 +28,7 @@ export class AdminGuardService implements CanActivate {
             return this.router.createUrlTree(['/sign-in']);
           }
 
-          
+
           if(!user.profileComplete && route.routeConfig.path != 'edit-profile'){
             return this.router.createUrlTree(['edit-profile']);
           }
@@ -40,6 +41,9 @@ export class AdminGuardService implements CanActivate {
           if(route.data.accessLevel!= null && route.data.accessLevel <= userLevel){
             // //users access level is equal to that of allowed by this route
             return true;
+          } else if (route.data.accessLevel!= null && route.data.accessLevel > userLevel) {
+            this.errorService.passError('You Can Not Access This Page!', '/home');
+            return false
           }
 
           return this.router.createUrlTree(['/']);
